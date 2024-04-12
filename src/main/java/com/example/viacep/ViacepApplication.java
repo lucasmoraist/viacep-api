3f647648ac58289979f3ab8e1a54a2f117fd4207
lucasmoraist <luksmnt1101@gmail.com>
2024-04-12 10:44:01 -0300

    feat: adicionando OpenFeign no projeto

--- src/main/java/com/example/viacep/ViacepApplication.java
+++ src/main/java/com/example/viacep/ViacepApplication.java
@@ -3,18 +3,16 @@ package com.example.viacep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ViacepApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ViacepApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
 }
